309135d0ff9715d69a4263d818ad332b2f706d7c
clckvz <clckvz@gmail.com>
2024-09-13 08:17:58 -0300

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,1 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCoQj6rIpLQf65yvyB14rzAfSAQ62-r3bA",
+    authDomain: "cprp-shop.firebaseapp.com",
+    projectId: "cprp-shop",
+    storageBucket: "cprp-shop.appspot.com",
+    messagingSenderId: "500688642149",
+    appId: "1:500688642149:web:0174d8965f2b262fc5668c"
+  };
 
+  firebase.initializeApp(firebaseConfig);
